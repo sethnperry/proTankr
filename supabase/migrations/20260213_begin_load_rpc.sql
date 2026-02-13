19bb9588209775eff21daebf3ad98697879cf836
Seth Perry <sethnperry@gmail.com>
2026-02-13 12:29:53 -0500

    Add begin_load and complete_load RPCs

---
+++ supabase/migrations/20260213_begin_load_rpc.sql
@@ -0,0 +1,19 @@
+{
+  "combo_id": "uuid",
+  "terminal_id": "uuid",
+  "state_code": "FL",
+  "city_id": "uuid",
+  "cg_bias": 0.0,
+  "ambient_temp_f": 72,
+  "product_temp_f": 65,
+  "planned_totals": {
+    "planned_total_gal": 7800,
+    "planned_total_lbs": 52000,
+    "planned_gross_lbs": 78000
+  },
+  "planned_snapshot": { "...anything you want..." },
+  "lines": [
+    { "comp_number": 1, "product_id": "uuid", "planned_gallons": 2500, "planned_lbs": 16700, "temp_f": 65 },
+    { "comp_number": 2, "product_id": "uuid", "planned_gallons": 2600, "planned_lbs": 17300, "temp_f": 65 }
+  ]
+}
---
+++ supabase/migrations/20260213_complete_load_rpc.sql
@@ -0,0 +1,13 @@
+{
+  "load_id": "uuid",
+  "lines": [
+    {
+      "comp_number": 1,
+      "actual_gallons": 2490,
+      "actual_lbs": 16610,
+      "temp_f": 66,
+      "api_60": 60.3,
+      "alpha_per_f": 0.00043
+    }
+  ]
+}
